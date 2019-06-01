c35632440a7ee3bf629ddb9d1300f18d30ea21e9
michaeldai1006 <dtc74110@gmail.com>
2019-05-31 19:02:45 -0700

    Initial Commit

---
+++ tdtest_mapkit_route/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tdtest_mapkit_route
+//
+//  Created by OD5 on 5/31/19.
+//  Copyright © 2019 tdtest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
